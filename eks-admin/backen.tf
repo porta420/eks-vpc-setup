0df00d799ebdaeb84d3b8379c3f93b5a7e59cda0
noeldevops <noelkin373@gmail.com>
2025-08-09 22:14:55 +0200

    eks admin

--- eks-admin/backen.tf
+++ eks-admin/backen.tf
@@ -1,10 +1,18 @@
 terraform {
   backend "s3" {
     bucket = "noel-s3-tf-state-bucket"
-    key    = "eks-cluster/terraform.tfstate"
+    key    = "eksadmin/terraform.tfstate"
     region = "eu-north-1"
 
     # Uncomment below if you want DynamoDB locking
     # dynamodb_table = "terraform-locks"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
